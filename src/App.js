32ddbd8b64fd21e5d1d3629299450553baba0bdd
Akhila Vijayan <akhilavijayan964@gmail.com>
2022-07-22 16:25:10 +0530

    Netflix logo, avatar , navbar done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from 'react'
+import NavBar from './Components/Navbar/NavBar';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <NavBar/>
     </div>
   );
 }
