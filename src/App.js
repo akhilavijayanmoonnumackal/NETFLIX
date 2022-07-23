2a98e5c2a3d9ef919cb7c2380fd681cc4a00931f
Akhila Vijayan <akhilavijayan964@gmail.com>
2022-07-23 16:25:29 +0530

    posters with different types and url completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import NavBar from './Components/Navbar/NavBar';
+import {originals,action,ComedyMovies,HorrorMovies,RomanceMovies,Documentaries} from './urls'
 import './App.css'
 import Banner from './Components/Banner/Banner';
 import RowPost from './Components/RowPost/RowPost';
@@ -9,7 +10,14 @@ function App() {
     <div className="App">
       <NavBar/>
       <Banner/>
-      <RowPost/>
+      <RowPost url={originals} title='Netflix Originals'/>
+      <RowPost url={action} title='Action' isSmall />
+      <RowPost url={ComedyMovies} title='ComedyMovies' isSmall />
+      <RowPost url={HorrorMovies} title='HorrorMovies' isSmall />
+      <RowPost url={RomanceMovies} title='RomanceMovies' isSmall />
+      <RowPost url={Documentaries} title='Documentaries' isSmall />
+
+
     </div>
   );
 }
