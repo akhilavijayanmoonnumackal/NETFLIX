fbbf8ce299638b0374d78626f9e7153316fc68f9
Akhila Vijayan <akhilavijayan964@gmail.com>
2022-07-23 21:27:56 +0530

    Image id passed, Youtube trailer

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,10 +1,12 @@
 import React,{useEffect,useState} from 'react'
+import YouTube from 'react-youtube'
 import './RowPost.css'
-import {imageUrl} from '../../constants/constants'
+import {imageUrl,API_KEY} from '../../constants/constants'
 import axios from '../../axios'
 
 function RowPost(props) {
   const [movies,setmovies] = useState([])
+  const [urlId,setUrlId]=useState('')
   useEffect(()=>{
     axios.get(props.url).then(response=>{
       console.log(response.data)
@@ -13,18 +15,37 @@ function RowPost(props) {
       //alert('Network Error')
     })
   },[])
+
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+  const handleMovie = (id)=>{
+    console.log(id)
+    axios.get(`movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response=>{
+      if(response.data.results.length!==0){
+        setUrlId(response.data.results[0])
+      }else{
+        console.log('Array Empty')
+      }
+    })
+  }
+
   return (
     <div className='row'>
       <h2>{props.title}</h2>
       <div className='posters'>
         {movies.map((obj)=>
-          <img className={props.isSmall ? 'smallPoster' : 'poster'} src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
+          <img onClick={()=>handleMovie(obj.id)} className={props.isSmall ? 'smallPoster' : 'poster'} src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
 
-        )}
-        
-        
+        )}       
 
       </div>
+    { urlId &&  <YouTube opts={opts} videoId={urlId.key} />  }
     </div>
   )
 }
